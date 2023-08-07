b866f358a752a740421b382b0a23112901a5b90a
Hanna <hmbdelapena@gmail.com>
2023-08-07 13:03:48 +0800

    Write a view "DeactivateOrderView" that sets the is_active state on an order.

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
@@ -13,3 +14,14 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class DeactivateOrderView(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False  # Set is_active to False to deactivate
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
