0038d9532ae7890ccbc5f7ed0aad87d38fe02530
Hanna <hmbdelapena@gmail.com>
2023-08-07 12:54:21 +0800

    Create a view that lists inventory items created after a certain day.

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from django.utils import timezone
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +14,16 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class InventoryItemListAfterDate(generics.ListCreateAPIView):
+    serializer_class = OrderTagSerializer
+
+    def get_queryset(self):
+        start_date_str = self.request.query_params.get('start_date')
+        if start_date_str:
+            try:
+                start_date = timezone.datetime.strptime(start_date_str, '%Y-%m-%d')
+                return OrderTag.objects.filter(start_date__gt=start_date)
+            except ValueError:
+                pass
+        return OrderTag.objects.none()    
